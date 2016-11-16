52c7e4dfe1df334df8a6899e8ce71f17173fcea6
ehost-phonglt <phonglt@ehost.com.vn>
2016-11-16 16:25:07 +0700

    them moi

---
+++ copy/js/pages/plans-a21b158908.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function() {
+  $('.marketing-block__side-menu__body').scrollNav({
+    sections: '.marketing-block-menu__title',
+    subSections: false,
+    sectionElem: 'section',
+    showHeadline: false,
+    showTopLink: false,
+    scrollToHash: true,
+    scrollOffset: 150,
+    animated: true,
+    speed: 200
+  });
+});
+
